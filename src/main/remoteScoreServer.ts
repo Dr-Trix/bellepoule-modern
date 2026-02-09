7b2215c5c55aabdcedbb6b0603c55ff0697d3aeb
Klinnex <yann.deboeuf@gmail.com>
2026-02-09 12:48:18 +0100

    Refonte complète du système de saisie distante
    - Nouvelle page d'accueil avec liste des arènes disponibles
    - Interface de saisie style SmartPoule avec boutons +1, +3, +5
    - Affichage déporté par arène avec scores rouge/vert et timer
    - WebSocket temps réel pour synchronisation
    - Routes mises à jour pour supporter les nouvelles pages
    
    Pages disponibles:
    - /home - Page d'accueil
    - /referee.html?arena=arena1 - Saisie scores
    - /arena-display.html?arena=arena1 - Affichage déporté

--- src/main/remoteScoreServer.ts
+++ src/main/remoteScoreServer.ts
@@ -179,37 +179,53 @@ export class RemoteScoreServer {
         : path.join(__dirname, '../remote', filename);
     };
 
+    // Page d'accueil avec liste des arènes
+    this.app.get('/home', (req, res) => {
+      res.sendFile(getRemotePath('index.html'));
+    });
+
+    // Page de saisie des scores (interface arbitre)
+    this.app.get('/referee.html', (req, res) => {
+      res.sendFile(getRemotePath('referee.html'));
+    });
+
+    // Page d'affichage déporté
+    this.app.get('/arena-display.html', (req, res) => {
+      res.sendFile(getRemotePath('arena-display.html'));
+    });
+
+    // Routes legacy pour compatibilité
     this.app.get('/arena1', (req, res) => {
-      res.sendFile(getRemotePath('arena.html'));
+      res.redirect('/arena-display.html?arena=arena1');
     });
 
     this.app.get('/arena2', (req, res) => {
-      res.sendFile(getRemotePath('arena.html'));
+      res.redirect('/arena-display.html?arena=arena2');
     });
 
     this.app.get('/arena3', (req, res) => {
-      res.sendFile(getRemotePath('arena.html'));
+      res.redirect('/arena-display.html?arena=arena3');
     });
 
     this.app.get('/arena4', (req, res) => {
-      res.sendFile(getRemotePath('arena.html'));
+      res.redirect('/arena-display.html?arena=arena4');
     });
 
-    // Interface d'arbitrage
+    // Interface d'arbitrage legacy
     this.app.get('/arena1/referee', (req, res) => {
-      res.sendFile(getRemotePath('referee.html'));
+      res.redirect('/referee.html?arena=arena1');
     });
 
     this.app.get('/arena2/referee', (req, res) => {
-      res.sendFile(getRemotePath('referee.html'));
+      res.redirect('/referee.html?arena=arena2');
     });
 
     this.app.get('/arena3/referee', (req, res) => {
-      res.sendFile(getRemotePath('referee.html'));
+      res.redirect('/referee.html?arena=arena3');
     });
 
     this.app.get('/arena4/referee', (req, res) => {
-      res.sendFile(getRemotePath('referee.html'));
+      res.redirect('/referee.html?arena=arena4');
     });
 
     this.app.post('/api/referees', (req, res) => {
